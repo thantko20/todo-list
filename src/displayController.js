5117b6fd72e25786865630b394f012960690c363
Thant Ko Zaw <thantmkou10@gmail.com>
2021-12-10 14:21:53 +0630

    Add clearChildNodes

--- src/displayController.js
+++ src/displayController.js
@@ -1,7 +1,19 @@
+import elementsModels from "./elementModels";
+
 class DisplayController {
   constructor() {}
 
-  
+  clearChildrenNodes(parentNode) {
+    while(parentNode.firstChild) parentNode.removeChild(parentNode.firstChild);
+  }
+
+  renderProjectTabs(projects) {
+    const projectsWrapper = document.querySelector('.projects-wrapper');
+    this.clearChildrenNodes(projectsWrapper);
+    projects.forEach(project => {
+      projectsWrapper.appendChild(elementsModels.projectTabModel(project.name, projects.indexOf(project)));
+    });
+  }
 }
 
 export default DisplayController;
